51ed5997d997e6b51d1472b26cf31e49d8c17bdd
AndreySh <shingaryovan@gmail.com>
2023-01-29 17:28:54 +1200

    программа Кегли написана, использованн тройной цикл

--- ex02/Program.cs
+++ ex02/Program.cs
@@ -13,7 +13,7 @@ Console.Write("Введите количество Бросков шара: ");
 int K = int.Parse(Console.ReadLine());              //это колиество бросков шара
 
 int Cagle_num = 0;                                  //переменная для сравнения
-for (Cagle_num = 1; Cagle_num <= N; Cagle_num++)     //это ряд кеглей
+for (Cagle_num = 0; Cagle_num < N; Cagle_num++)     //это ряд кеглей, наполняем его
 {
     array[Cagle_num] = "I";
 }
@@ -27,12 +27,24 @@ void PrintArray(string[] arg)
     }
     Console.WriteLine();
 }
+Console.Write("Это ряд кеглей до прилета шаров: ");
 PrintArray(array);
 
-string Ball_num = "Ball";
-
-for (int i = 1; i <= K; i++)
+for (int i = 1; i <= K; i++)                //этот цикл "запускает шары"
 {
-    Ball_num = "Ball" + i;
-    Console.Write(Ball_num + " ");          //показать номера шаров
+    int Li = new Random().Next(1, N+1);
+    int Ri = new Random().Next(1, N+1);
+    if (Li > Ri) (Li, Ri) = (Ri, Li);       //т.к. в условии сказано что Li меньше Ri то проверем и меняем их местами, если надо
+    Console.Write("Попадания шара " + i + ": ");
+    for (int j = Li; j <= Ri; j++)          //показываем все значения сбитых кеглей этого шара
+    {
+        Console.Write($"{j} ");
+        for (Cagle_num = 1; Cagle_num < N; Cagle_num++)
+        {
+            if (Cagle_num == j) array[Cagle_num - 1] = "."; //если шар попал, то "роняем" Кеглю
+        }
+    }
+    Console.WriteLine();
 }
+Console.Write("А это ряд кеглей после прилета шаров: ");
+PrintArray(array);
